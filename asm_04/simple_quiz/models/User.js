4b6e061fb8e455fea92fcf879087297712cf7898
huypg-bi <phamgiahuy2707@gmail.com>
2026-06-24 08:16:54 +0700

    refactor: remove old BE files from root

--- asm_04/simple_quiz/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-    username: { type: String, default: "" },
-    password: { type: String, required: true },
-    admin: { type: Boolean, default: false }
-});
-
-module.exports = mongoose.model('User', UserSchema);
